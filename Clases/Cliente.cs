c394dfac4b31e5f017bc72f47909cede6c30a031
FerrariFranco <123975956+FerrariFranco@users.noreply.github.com>
2023-09-30 16:05:25 -0300

    Comenzamos con mas clases

---
+++ Clases/Cliente.cs
@@ -0,0 +1,29 @@
+﻿using Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class Cliente : Usuario
+    {
+        private string direccion;
+        private string telefono;
+        private List<Volquete> carrito;
+
+
+        public Cliente(string nombreUsuario, string mailUsuario, string claveUsuario, string direccion, string telefono, List<Volquete> carrito)
+            : base(nombreUsuario, mailUsuario, claveUsuario)
+        {
+            this.Direccion = direccion;
+            this.Telefono = telefono;
+            this.Carrito = carrito;
+        }
+
+        public string Direccion { get => direccion; set => direccion = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
+        public List<Volquete> Carrito { get => carrito; set => carrito = value; }
+    }
+}
---
+++ Clases/Volquete.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+
+    public class Volquete
+    {
+        private float capacidad;
+        private string sedeActual;
+
+        public Volquete(float capacidad, string sedeActual)
+        {
+            this.Capacidad = capacidad;
+            this.SedeActual = sedeActual;
+        }
+
+        public float Capacidad { get => capacidad; set => capacidad = value; }
+        public string SedeActual { get => sedeActual; set => sedeActual = value; }
+    }
+}
