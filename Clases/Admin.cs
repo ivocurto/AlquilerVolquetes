c36acf27473bfb9e77455b19c6023bd7ab18bf0e
FerrariFranco <123975956+FerrariFranco@users.noreply.github.com>
2023-10-05 21:10:13 -0300

    Agregando Registros Admins

---
+++ Clases/Admin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class Admin : Usuario
+    {
+        private string rol;
+
+        public Admin(string nombreUsuario, string mailUsusario, string claveUsuario, string rol)
+            : base(nombreUsuario, mailUsusario, claveUsuario)
+        {
+            Rol = rol;
+        }
+
+        public string Rol { get => rol; set => rol = value; }
+    }
+}
